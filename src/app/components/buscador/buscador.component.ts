d1b074bb00982fa9bc0249e82b737aaa634887ea
Elver Tobo <eltobora@poligran.edu.co>
2020-10-13 22:08:40 -0500

    El buscador pasa la entrada automáticamente (on keyUp)

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSearch(value: string) {
+    if (value && value.length > 3) {
+      this.router.navigate(['/informacion'], {
+        queryParams: { q: value },
+      });
+    }
   }
 
 }
